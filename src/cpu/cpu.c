c7a5a5b57c393878766de4726b5da45b55adeb7e
qeeg <mszoopers@protonmail.com>
2022-04-29 16:15:46 -0500

    OOPS, made a mistake when reenabling PGE for the Pentium 3

--- src/cpu/cpu.c
+++ src/cpu/cpu.c
@@ -1367,7 +1367,7 @@ cpu_set(void)
 
                 cpu_features = CPU_FEATURE_RDTSC | CPU_FEATURE_MSR | CPU_FEATURE_CR4 | CPU_FEATURE_VME | CPU_FEATURE_MMX | CPU_FEATURE_SSE;
                 msr.fcr = (1 << 8) | (1 << 9) | (1 << 12) |  (1 << 16) | (1 << 19) | (1 << 21);
-                cpu_CR4_mask = CR4_VME | CR4_PVI | CR4_TSD | CR4_DE | CR4_PSE | CR4_MCE | CR4_PAE | CR4_PCE;
+                cpu_CR4_mask = CR4_VME | CR4_PVI | CR4_TSD | CR4_DE | CR4_PSE | CR4_MCE | CR4_PAE | CR4_PCE | CR4_PGE;
 	            cpu_CR4_mask |= CR4_OSFXSR;
 
 #ifdef USE_DYNAREC
