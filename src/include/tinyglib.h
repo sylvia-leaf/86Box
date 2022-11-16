05459741581b6e3bc2bb7fb1a722afcf7d7ee96e
Jasmine Iwanek <jasmine@iwanek.co.uk>
2022-11-16 01:50:10 -0500

    Reimplement part of tinyglib to fix a compile error

---
+++ src/include/tinyglib.h
@@ -0,0 +1,34 @@
+/*
+ * 86Box    A hypervisor and IBM PC system emulator that specializes in
+ *          running old operating systems and software designed for IBM
+ *          PC systems and compatibles from 1981 through fairly recent
+ *          system designs based on the PCI bus.
+ *
+ *          This file is part of the 86Box distribution.
+ *
+ *          Minimal reimplementation of GLib for libslirp.
+ *
+ *
+ *
+ * Authors: RichardG, <richardg867@gmail.com>
+ *
+ *          Copyright 2020 RichardG.
+ */
+#ifndef TINYGLIB_H
+# define TINYGLIB_H
+
+#include <glib.h>
+/* Types */
+
+/* Windows does not define ssize_t, so we need to define it here. */
+#ifndef _SSIZE_T_DEFINED
+# define _SSIZE_T_DEFINED
+# undef ssize_t
+# ifdef _WIN64
+#  define ssize_t int64_t
+# else
+#  define ssize_t int32_t
+# endif
+#endif
+
+#endif
