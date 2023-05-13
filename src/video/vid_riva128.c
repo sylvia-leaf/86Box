11159d2d4e5f1f771b814c7d16c2bb50230bb101
qeeg <mszoopers@protonmail.com>
2023-05-13 07:15:23 -0500

    Update vid_riva128.c

--- src/video/vid_riva128.c
+++ src/video/vid_riva128.c
@@ -1,4 +1,4 @@
-//License: GPL-2.0-or-later
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  * 86Box	A hypervisor and IBM PC system emulator that specializes in
  *		running old operating systems and software designed for IBM
