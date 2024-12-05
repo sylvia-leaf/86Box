3b363c2e94253c5d010edbc54272563878dd9fb3
fuel-pcbox <mszoopers@protonmail.com>
2024-12-04 18:25:11 -0600

    Fix MOVDQ2Q/MOVQ2DQ decoding

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -609,7 +609,7 @@ extern int hasfpu;
 #define CPU_FEATURE_SSE2    (1 << 11)
 #define CPU_FEATURE_CLFLUSH (1 << 12)
 #define CPU_FEATURE_NX      (1 << 13)
-#define CPU_FEATURE_SSE3      (1 << 14)
+#define CPU_FEATURE_SSE3    (1 << 14)
 
 extern uint64_t cpu_features;
 
