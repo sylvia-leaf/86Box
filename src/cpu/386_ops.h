3ebcd2c1d55851350c1f6a1c71fc462ce262f5d9
fuel-pcbox <mszoopers@protonmail.com>
2025-01-17 12:16:32 -0600

    Fix ARM64 macOS build

--- src/cpu/386_ops.h
+++ src/cpu/386_ops.h
@@ -199,7 +199,6 @@ extern void x386_dynarec_log(const char *fmt, ...);
 #include "x87_ops.h"
 #ifndef OPS_286_386
 #    include "softfloat3e/softfloat-compare.h"
-#    include "softfloat3e/softfloat-specialize.h"
 #    include "softfloat3e/fpu_trans.h"
 #    include "x86_ops_i686.h"
 #    include "x86_ops_mmx.h"
--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -222,8 +222,6 @@ typedef struct {
     int      checked; /*Non-zero if selector is known to be valid*/
 } x86seg;
 
-#include "softfloat3e/softfloat_types.h"
-
 typedef union {
     uint64_t q;
     int64_t  sq;
@@ -245,8 +243,8 @@ typedef union {
     int16_t  sw[8];
     uint8_t  b[16];
     int8_t   sb[16];
-    float32   f[4];
-    float64   d[2];
+    uint32_t   f[4]; //float32
+    uint64_t   d[2]; //float64
     float f2[4];
     double d2[2];
 } SSE_REG;
