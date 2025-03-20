9b9db70b05b1a645613bde53e714bbe94c7a63ba
fuel-pcbox <mszoopers@protonmail.com>
2025-03-20 12:01:05 -0500

    Oops

--- src/mem/mem.c
+++ src/mem/mem.c
@@ -462,7 +462,7 @@ mmutranslatereal_pae(uint32_t addr, int rw)
         return 0xffffffffffffffffULL;
     }
 
-    f(nxbit && (rw == 2)) {
+    if(nxbit && (rw == 2)) {
         cr2 = addr;
         temp &= 1;
         temp |= 0x10;
