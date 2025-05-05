f841c354be6aff46d13141af0116035bb49d9291
fuel-pcbox <mszoopers@protonmail.com>
2025-05-04 23:00:40 -0500

    Fix merge

--- src/cpu/x86_ops_mmx.h
+++ src/cpu/x86_ops_mmx.h
@@ -46,11 +46,6 @@
         x86_int(7);                          \
         return 1;                            \
     }                                        \
-    if (cr0 & 0x4) {                         \
-        cpu_state.pc = cpu_state.oldpc;       \
-        x86illegal();                         \
-        return 1;                            \
-    }                                        \
     x87_set_mmx()
 
 #define SSE_ENTER()  \
