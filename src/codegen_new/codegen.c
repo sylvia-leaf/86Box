5d3b83c753efb91c5692102029781e153e40501c
fuel-pcbox <mszoopers@protonmail.com>
2025-05-17 16:13:01 -0500

    Attempt to fix SSE2 NDR again lmao

--- src/codegen_new/codegen.c
+++ src/codegen_new/codegen.c
@@ -771,7 +771,7 @@ generate_call:
     uop_CALL_INSTRUCTION_FUNC(ir, op);
     uop_MOV_IMM(ir, IREG_sse_xmm, 0);
     codegen_flags_changed = 0;
-    codegen_mark_code_present(block, cs + cpu_state.pc, 8);
+    codegen_mark_code_present(block, cs + cpu_state.pc, 10);
 
     last_op_32     = op_32;
     last_op_ea_seg = op_ea_seg;
