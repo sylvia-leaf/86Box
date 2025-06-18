384bef95f0ab10b5d1ad824f80cb0ec2d90150df
fuel-pcbox <mszoopers@protonmail.com>
2025-06-18 07:45:23 -0500

    More SSE fixes

--- src/cpu/x86_ops_sse_cmp.h
+++ src/cpu/x86_ops_sse_cmp.h
@@ -11,7 +11,7 @@ opUCOMISS_xmm_xmm_a16(uint32_t fetchdat)
     fetch_ea_16(fetchdat);
     SSE_REG src;
     struct softfloat_status_t status = mxcsr_to_softfloat_status_word();
-    SSE_GETSRC();
+    SSE_GETSRC_NOALIGN();
     cpu_state.flags &= ~(V_FLAG | A_FLAG | N_FLAG | Z_FLAG | P_FLAG | C_FLAG);
     int relation = f32_compare_quiet(cpu_state.XMM[cpu_reg].f[0], src.f[0], &status);
     FPU_write_eflags_fpu_compare(relation);
@@ -38,7 +38,7 @@ opUCOMISS_xmm_xmm_a32(uint32_t fetchdat)
     fetch_ea_32(fetchdat);
     SSE_REG src;
     struct softfloat_status_t status = mxcsr_to_softfloat_status_word();
-    SSE_GETSRC();
+    SSE_GETSRC_NOALIGN();
     cpu_state.flags &= ~(V_FLAG | A_FLAG | N_FLAG | Z_FLAG | P_FLAG | C_FLAG);
     int relation = f32_compare_quiet(cpu_state.XMM[cpu_reg].f[0], src.f[0], &status);
     FPU_write_eflags_fpu_compare(relation);
@@ -65,7 +65,7 @@ opCOMISS_xmm_xmm_a16(uint32_t fetchdat)
     fetch_ea_16(fetchdat);
     SSE_REG src;
     struct softfloat_status_t status = mxcsr_to_softfloat_status_word();
-    SSE_GETSRC();
+    SSE_GETSRC_NOALIGN();
     cpu_state.flags &= ~(V_FLAG | A_FLAG | N_FLAG | Z_FLAG | P_FLAG | C_FLAG);
     int relation = f32_compare_normal(cpu_state.XMM[cpu_reg].f[0], src.f[0], &status);
     FPU_write_eflags_fpu_compare(relation);
@@ -92,7 +92,7 @@ opCOMISS_xmm_xmm_a32(uint32_t fetchdat)
     fetch_ea_32(fetchdat);
     SSE_REG src;
     struct softfloat_status_t status = mxcsr_to_softfloat_status_word();
-    SSE_GETSRC();
+    SSE_GETSRC_NOALIGN();
     cpu_state.flags &= ~(V_FLAG | A_FLAG | N_FLAG | Z_FLAG | P_FLAG | C_FLAG);
     int relation = f32_compare_normal(cpu_state.XMM[cpu_reg].f[0], src.f[0], &status);
     FPU_write_eflags_fpu_compare(relation);
