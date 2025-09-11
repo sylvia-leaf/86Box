ed2d4c90338347d63dfc83ef53dad9ad1deb5e8a
fuel-pcbox <mszoopers@protonmail.com>
2025-09-11 16:03:17 -0500

    Turn on MMX recompilation except for CPUs with SSE2.

--- src/codegen_new/codegen.c
+++ src/codegen_new/codegen.c
@@ -465,7 +465,7 @@ codegen_generate_call(uint8_t opcode, OpFn op, uint32_t fetchdat, uint32_t new_p
 #endif
                 is_0f = 1;
                 op_table        = x86_dynarec_opcodes_0f;
-                recomp_op_table = recomp_opcodes_0f_no_mmx;
+                recomp_op_table = (fpu_softfloat || (cpu_features & CPU_FEATURE_SSE2)) ? recomp_opcodes_0f_no_mmx : recomp_opcodes_0f;
                 if(is_repe)
                 {
                     op_table        = x86_dynarec_opcodes_REPE_0f;
